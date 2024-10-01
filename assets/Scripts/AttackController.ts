0e7f1ea7a5ec045fa90a580295b8158a47e1f4da
LEIGENGEN <905627019@qq.com>
2024-10-01 21:53:29 +0800

    根据子弹prefab实例和点击触发创建多个球体

--- assets/Scripts/AttackController.ts
+++ assets/Scripts/AttackController.ts
@@ -1,22 +1,20 @@
-import { _decorator, Component, Node, Prefab } from 'cc';
+import { _decorator, Component, Node, Prefab, input, Input, EventTouch, instantiate } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('AttackController')
 export class AttackController extends Component {
-    @property
-    public maxNumberOfAttack: number = 100 //默认首字母大写，初始值会识别，如果冲突会以cocos面板为准 property是添加进去
-
-    @property
-    public testString: string = 'hello'
-
-    @property(Node)
-    public ground: Node = null
-
+    //默认首字母大写，初始值会识别，如果冲突会以cocos面板为准 property是添加进去
     @property(Prefab)
     public bulletPrefab: Prefab = null
+    // 预制体prefab 实例instance
 
     start() {
+        input.on(Input.EventType.TOUCH_START, this.onTouchStart, this)
+    }
 
+    onTouchStart(event: EventTouch) {
+        const bullet = instantiate(this.bulletPrefab)//访问自身属性的时候要使用this
+        bullet.setParent(this.node)
     }
 
     update(deltaTime: number) {
