9959748fea77d9d99ab61a264b7872dc16c7b302
LEIGENGEN <905627019@qq.com>
2024-10-01 20:37:14 +0800

    触摸事件控制相机移动

--- assets/Scripts/CameraController.ts
+++ assets/Scripts/CameraController.ts
@@ -6,21 +6,22 @@ const { ccclass, property } = _decorator;
 export class Test extends Component {
   start() {
     input.on(Input.EventType.TOUCH_START, this.onTouchStart, this)
-    input.on(Input.EventType.TOUCH_MOVE,this.onTouchMove,this)
-    input.on(Input.EventType.TOUCH_END,this.onTouchEnd,this)
+    input.on(Input.EventType.TOUCH_MOVE, this.onTouchMove, this)
+    input.on(Input.EventType.TOUCH_END, this.onTouchEnd, this)
   }
 
-  onTouchStart(event:EventTouch) {
-    console.log('touchStart',event.getLocation());
+  onTouchStart(event: EventTouch) {
+    console.log('touchStart', event.getLocation());
   }
 
-  onTouchMove(event:EventTouch) {
-    console.log('touchStart',event.getLocation());
+  onTouchMove(event: EventTouch) {
+    const moveScale = 0.05
+    const pos = this.node.position
+    this.node.setPosition(pos.x + event.getDeltaX() * moveScale, pos.y + event.getDeltaY() * moveScale, pos.z)
   }
 
-  onTouchEnd(event:EventTouch) {
-    console.log('touchStart',event.getLocation());
-    console.log('touchStart',event.getDelta());
+  onTouchEnd(event: EventTouch) {
+    console.log('touchStart', event.getLocation());
   }
 
   update(deltaTime: number) {
